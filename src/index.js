2f77d8b8f52bf7a51d1639dc263b55dd8c502ce4
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-14 13:00:51 +0200

    ejemplo contador con clases

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Components/App/App';
+import Contador from './Components/Contador'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Contador inicio="4"/>
+    <Contador inicio="9"/>
   </React.StrictMode>
 );
 

41716440ac78b0050471f8aa9abaec735da25ba6
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-15 09:22:38 +0200

    Beautify deportes

--- src/Components/HijoDeporte.js
+++ src/Components/HijoDeporte.js
@@ -1,21 +1,16 @@
 import { Component } from 'react'
 
 class HijoDeporte extends Component {
-  state = {
-    favorito: ""
-  }
-  mostrarFavorito = () => {
-    this.setState({
-      favorito: this.props.nombre
-    })
+  seleccionarFavorito = () => {
+    //CAPTURAMOS EL DEPORTE DEL PROPIO COMPONENT EN PROPS
+    var deporte = this.props.nombre;
+    //REALIZAMOS LA PETICION AL PADRE
+    this.props.mostrarFavorito(deporte);
   }
   render() {
     return (<div>
       <h3 style={{ color: "blue" }}>Deporte: {this.props.nombre}</h3>
-      <h2 style={{ backgroundColor: "yellow" }}>
-        Su deporte favorito es: {this.state.favorito}
-      </h2>
-      <button onClick={this.mostrarFavorito}>Favorito</button>
+      <button onClick={this.seleccionarFavorito}>Seleccionar favorito</button>
     </div>)
   }
 }
